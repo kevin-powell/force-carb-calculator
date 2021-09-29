b95f7f23c2f964dbde8918f3f316c975d1e6ead7
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2021-09-29 12:54:07 +0100

    Refactor

--- main.js
+++ main.js
@@ -4,36 +4,16 @@ const tempInput = document.querySelector("#temp");
 const timeInput = document.querySelector("#time");
 const output = document.querySelector("output");
 
-inputs.forEach((input) => {
-  validateInput(input);
-  input.oninput = outputResult;
-});
+function handleInput(evt) {
+  const input = evt.target;
 
-function setInitialValues() {
-  pressureInput.value = localStorage.getItem("pressure") ?? 12;
-  tempInput.value = localStorage.getItem("temp") ?? 36;
-  timeInput.value = localStorage.getItem("time") ?? 120;
-}
-
-function storeUserValues() {
-  localStorage.setItem("pressure", pressureInput.value);
-  localStorage.setItem("temp", tempInput.value);
-  localStorage.setItem("time", timeInput.value);
-}
-
-function validateInput(input) {
-  input.addEventListener("beforeinput", function (e) {
-    let beforeValue = input.value;
-    e.target.addEventListener(
-      "input",
-      function () {
-        if (input.validity.badInput) {
-          input.value = beforeValue;
-        }
-      },
-      { once: true }
-    );
-  });
+  if(input.validity.valid) {
+    localStorage.setItem(input.name, input.value);
+    outputResult();
+  }
+  else {
+    input.value = localStorage.getItem(input.name) ?? input.getAttribute("value");
+  }
 }
 
 function outputResult() {
@@ -46,9 +26,13 @@ function outputResult() {
     (0.17 * pa * (1 - e ** (-0.014 * t)) + 3.04) *
     e ** ((0.0006 * Math.log(pa * (1 - e ** (-0.014 * t))) - 0.02) * temp);
 
-  storeUserValues();
-  output.innerHTML = result.toFixed(2);
+  output.textContent = result.toFixed(2);
 }
 
-setInitialValues();
+
+inputs.forEach(input => {
+  input.value = localStorage.getItem(input.name) ?? input.getAttribute("value");
+  input.oninput = handleInput;
+});
+
 outputResult();
