16e20370b7846824a0be77e2b45af9911d87a934
Kevin Powell <hi@kevinpowell.co>
2021-11-08 13:02:53 -0500

    it works!

--- main.js
+++ main.js
@@ -1,34 +1,47 @@
-const inputs = document.querySelectorAll("input");
+const inputs = document.querySelectorAll("#main-controls input");
 const pressureInput = document.querySelector("#pressure");
 const tempInput = document.querySelector("#temp");
 const timeInput = document.querySelector("#time");
 const output = document.querySelector("output");
 const unitToggleContainer = document.querySelector(".unit-toggle");
 const unitToggleButton = unitToggleContainer.querySelector("button");
+const unitToggleLabels = unitToggleContainer.querySelectorAll("label");
+// const celInputLabel = unitToggleContainer.querySelector('[for="cel"]');
+// const fahInputLabel = unitToggleContainer.querySelector('[for="fah"]');
 const celInput = unitToggleContainer.querySelector("#cel");
 const fahInput = unitToggleContainer.querySelector("#fah");
-
-unitToggleButton.addEventListener("click", toggleUnit);
-celInput.addEventListener("click", toggleUnit(e));
-fahInput.addEventListener("click", toggleUnit(e));
+const unitLabel = document.querySelector("[data-unit]");
 
 inputs.forEach((input) => {
   validateInput(input);
   input.oninput = outputResult;
 });
 
+unitToggleButton.addEventListener("click", toggleButton);
+
+celInput.addEventListener("change", toggleUnit);
+fahInput.addEventListener("change", toggleUnit);
+
 function setInitialValues() {
   pressureInput.value = localStorage.getItem("pressure") ?? 12;
   tempInput.value = localStorage.getItem("temp") ?? 36;
   timeInput.value = localStorage.getItem("time") ?? 120;
-  // celInput.checked = localStorage.getItem("cel") === "true" ?? true;
+  celInput.checked = localStorage.getItem("cel") ?? false;
+
+  if (celInput.checked) {
+    unitLabel.innerText = "C";
+  }
 }
 
 function storeUserValues() {
   localStorage.setItem("pressure", pressureInput.value);
   localStorage.setItem("temp", tempInput.value);
   localStorage.setItem("time", timeInput.value);
-  // localStorage.setItem("cel", celInput.checked);
+  if (celInput.checked === true) {
+    localStorage.setItem("cel", celInput.checked);
+  } else {
+    localStorage.removeItem("cel");
+  }
 }
 
 function validateInput(input) {
@@ -53,7 +66,7 @@ function outputResult() {
   const t = timeInput.value;
 
   if (celInput.checked) {
-    temp = toFahrenheit(temp);
+    temp = unitsToFahrenheit(temp);
   }
 
   const result =
@@ -61,37 +74,54 @@ function outputResult() {
     e ** ((0.0006 * Math.log(pa * (1 - e ** (-0.014 * t))) - 0.02) * temp);
 
   storeUserValues();
-  output.innerHTML = result.toFixed(2);
+  output.innerText = result.toFixed(2);
 }
 
 function toggleUnit(e) {
-  e.preventDefault;
   const currentTemp = tempInput.value;
-  const unitLabel = document.querySelector("[data-unit]");
 
   if (celInput.checked) {
-    fahInput.checked = true;
-    tempInput.value = toFahrenheit(currentTemp);
-    unitToggleButton.setAttribute("aria-label", "switch to Celsius");
-    unitLabel.innerHTML = "F";
-  } else {
-    celInput.checked = true;
-    tempInput.value = toCelsius(currentTemp);
-    unitToggleButton.setAttribute("aria-label", "switch to Fahrenheit");
-    unitLabel.innerHTML = "C";
+    labelsToCelsius();
+    tempInput.value = unitsToCelsius(currentTemp);
+  } else if (fahInput.checked) {
+    labelsToFahrenheit();
+    tempInput.value = unitsToFahrenheit(currentTemp);
   }
-
   storeUserValues();
 }
 
-function toCelsius(value) {
+function toggleButton() {
+  const currentTemp = tempInput.value;
+  if (celInput.checked) {
+    fahInput.checked = true;
+    labelsToFahrenheit();
+    tempInput.value = unitsToFahrenheit(currentTemp);
+  } else if (fahInput.checked) {
+    celInput.checked = true;
+    labelsToCelsius();
+    tempInput.value = unitsToCelsius(currentTemp);
+  }
+  storeUserValues();
+}
+
+function unitsToCelsius(value) {
   const result = (value - 32) * (5 / 9);
   return result.toFixed(2);
 }
 
-function toFahrenheit(value) {
+function unitsToFahrenheit(value) {
   const result = (value * 9) / 5 + 32;
   return result.toFixed(2);
+}
+
+function labelsToCelsius() {
+  unitToggleButton.setAttribute("aria-label", "switch to Fahrenheit");
+  unitLabel.innerText = "C";
+}
+
+function labelsToFahrenheit() {
+  unitToggleButton.setAttribute("aria-label", "switch to Celsius");
+  unitLabel.innerText = "F";
 }
 
 setInitialValues();
